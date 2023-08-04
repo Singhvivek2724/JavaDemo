9634430095337e81e38ef93ca7497b9b4bf41ec4
Singhvivek2724 <rajputvivek8578@gmail.com>
2023-08-04 19:23:59 +0530

    second upload

---
+++ A.java
@@ -0,0 +1,13 @@
+
+import practice_01.Practice;
+public class A
+{
+public static void main (String args[])
+{
+	//Practice p=new Practice();
+	Practice p=new Practice();
+	
+	
+	
+}
+}
---
+++ B.java
@@ -0,0 +1,9 @@
+import Pack1.*    
+class B
+{    
+    public static void main(String …args)    
+{    
+    Demo ob1= new demo();    
+ob1.Show();    
+}    
+}
